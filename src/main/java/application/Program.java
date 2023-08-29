f782e6445116a048ccf18bc6827252fc472efe8d
Mateus Henrique <mateushbsb@gmail.com>
2023-08-29 17:08:28 -0300

    map

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,18 +1,28 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import domain.Pessoa;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(1, "Mangabão", "manga@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Trevor", "trevin@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Jorge", "balao@gmail.com");
+		Pessoa p1 = new Pessoa(null, "Mangabão", "manga@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Trevor", "trevin@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Jorge", "balao@gmail.com");
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Ok");
 	}
 
 }
